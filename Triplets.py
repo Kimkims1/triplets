9cbf04b22b715d837865d9e3fbc499754e428189
Kimkims1 <kimanthikevin76@gmail.com>
2020-03-03 00:09:39 +0300

    triplets

---
+++ Triplets.py
@@ -0,0 +1,10 @@
+def triplets(n):
+    if n>0:
+        for a in range(1, n-1):
+            for b in range(a+1, n):
+                for c in range(b+1,n+1):
+                    if a*a + b*b == c*c:
+                        print(a,b,c)
+
+
+triplets(20)
